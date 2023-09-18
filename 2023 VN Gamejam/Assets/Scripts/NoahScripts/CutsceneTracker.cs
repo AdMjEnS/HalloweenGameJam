35a4d9513b7fe8c5eece72697de48476fda80c34
F21H43 <fisher@heinsdesign.com>
2023-09-17 21:17:52 -0500

    Update CutsceneTracker.cs
    Functions added

--- 2023 VN Gamejam/Assets/Scripts/NoahScripts/CutsceneTracker.cs
+++ 2023 VN Gamejam/Assets/Scripts/NoahScripts/CutsceneTracker.cs
@@ -12,7 +12,7 @@ public class CutsceneTracker : MonoBehaviour
         Appear,             // Makes a gameObject sprite appear either in frame instantly or from off screen and move on screen
         Remove,             // Makes a gameObject sprite dissapear either in frame instantly or move off screen then disapear
         Shake,              // Shakes a gameObject ***For example play the yarn example VN to see what I mean
-        Rotate,             // Rotates a gameObject sprite on an axis so there facing the opposite direction
+        Mirror,             // Rotates a gameObject sprite on an axis so there facing the opposite direction
         Move,               // Moves a gameObject from where they are on the screen to another spot on the screen
         Backgound,          // Changes the background image
         ChangePose          // Swaps either the gameObject or sprite of something to something else, likely used for when we want to have a character change there body pose mide conversation
@@ -114,5 +114,67 @@ public class CutsceneTracker : MonoBehaviour
         continueText.enabled = true;
         yield return StartCoroutine(WaitForDownKey(KeyCode.Return));
         continueText.enabled = false;
+    }
+
+    void Appear(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+
+
+    void Remove(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+    /*
+    IEnumerator Appear(GameObject obj, Vector3 startPos, Vector3 endPos, float timeToMove)
+    {
+        transform.position = startPos;
+        gameObject.SetActive(true);
+        float elapsedTime = 0;
+        while (elapsedTime <= timeToMove)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, elapsedTime / timeToMove);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = endPos;
+    } */
+
+    void Shake()
+    {
+
+    }
+    void Mirror(RectTransform trans)
+    {
+        trans.rotation = new Quaternion(trans.rotation.x,trans.rotation.y + 180, trans.rotation.z, trans.rotation.w);
+    }
+    void Move(GameObject obj, Vector3 startPos, Vector3 endPos, float timeToMove)
+    {
+        StartCoroutine(MoveLerp(obj, startPos, endPos, timeToMove));
+    }
+
+    IEnumerator MoveLerp(GameObject obj, Vector3 startPos, Vector3 endPos, float timeToMove)
+    {
+        transform.position = startPos;
+        float elapsedTime = 0;
+        while (elapsedTime <= timeToMove)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, elapsedTime / timeToMove);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = endPos;
+    }
+
+    [SerializeField]
+    Image backgroundImage;
+    void Backgound(Sprite img)
+    {
+        ChangePose(backgroundImage, img);
+    }
+    void ChangePose(Image obj, Sprite img)
+    {
+        obj.sprite = img;
     }
 }
