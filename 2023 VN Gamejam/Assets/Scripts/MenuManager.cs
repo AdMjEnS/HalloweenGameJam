43a352be653250b16ef5c0b274f4b62de9a6aaf9
Kyron Larenz Patterson <bluekeshon79@gmail.com>
2023-09-29 20:44:16 -0500

    Added a ton of settings stuff

--- 2023 VN Gamejam/Assets/Scripts/MenuManager.cs
+++ 2023 VN Gamejam/Assets/Scripts/MenuManager.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MenuManager : MonoBehaviour
 {
     public Image viewImage1, viewImage2;
-    public List<Image> bGImages = new List<Image>();
+    public List<GameObject> allPanels = new List<GameObject>();
+    public AudioMixer audioMixer;
+
+    public float scrollSpeed = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetAllPanelsOff();
+
+        //audioMixer.SetFloat("masterVolume", PlayerPrefs.GetFloat("masterVolume"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ScrollBackgroundImages();
     }
 
     public void PlayGame(string sceneName)
@@ -33,6 +39,14 @@ public class MenuManager : MonoBehaviour
 
     private void ScrollBackgroundImages()
     {
-        
+        //viewImage1.transform.position = viewImage1.transform.position - new Vector3((viewImage1.transform.position.x * scrollSpeed), viewImage1.transform.position.y);
+    }
+
+    private void SetAllPanelsOff()
+    {
+        foreach(GameObject panel in allPanels)
+        {
+            panel.gameObject.SetActive(false);
+        }
     }
 }
---
+++ 2023 VN Gamejam/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    private Slider slider;
+
+    public string sliderName;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+
+        slider.value = PlayerPrefs.GetFloat(sliderName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLevel(float value)
+    {
+        mixer.SetFloat(sliderName, Mathf.Log10(value) * 20);
+
+        PlayerPrefs.SetFloat(sliderName, value);
+    }    
+}
