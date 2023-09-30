b0505098b1ffc79b93678c7e3c393a19d03992ca
AdMjEnS <75140685+AdMjEnS@users.noreply.github.com>
2023-09-30 13:22:08 -0500

    Quarter of the way done

--- 2023 VN Gamejam/Assets/Scripts/NoahScripts/CutsceneTracker.cs
+++ 2023 VN Gamejam/Assets/Scripts/NoahScripts/CutsceneTracker.cs
@@ -142,7 +142,7 @@ public class CutsceneTracker : MonoBehaviour
 
     public void Awake()
     {
-        StartCoroutine(VisualNovelSceneCurator(0));
+        StartCoroutine(VisualNovelSceneCurator(3));
     }
 
     public void Update()
